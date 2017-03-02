72dd6f2d008ce221f3d1d45792a639ea6571d97a
Murray Resinski <murray.resinski@octanner.com>
2017-03-02 21:53:22 +0000

    made start.sh executable  - removed extra stuff

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM quay.octanner.io/base/oct-ruby:2.3.1-onbuild
+EXPOSE 9000
 RUN chmod +x start.sh
 CMD ./start.sh
+
--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-#if [ -z "$COURIER_ENV" ]; then
-#echo You need to pass in COURIER_ENV
-#exit 1
-#else
-#bundle exec parallel_rspec spec/services/${COURIER_ENV}
-#fi
-
-#if [ $# -ne 0 ]; then
-
-#if
-#[ "$stage" ]; then ENV=$stage bundle exec parallel_rspec spec/services/${1};
-#fi
-
 bundle exec rails server -b 0.0.0.0 -p $PORT
